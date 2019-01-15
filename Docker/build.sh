be6bd829d131a4c3067fb3d33c07e8b4292566e7
Akapon Onnual <akkao130@infin.postbox.in.th>
2019-01-15 14:12:12 +0700

    Fix wrong image repo

--- Docker/build.sh
+++ Docker/build.sh
@@ -9,7 +9,7 @@ docker-compose -f $COMPOSE_FILE build
 docker-compose -f $COMPOSE_FILE push
 
 if [ "$TAG" != "latest" ]; then
-    docker tag vpcpqmregistry.azurecr.io/portfolio-api${TAG} vpcpqmregistry.azurecr.io/portfolio-api:latest
-    docker push vpcpqmregistry.azurecr.io/portfolio-api:latest
+    docker tag vpcpqmregistry.azurecr.io/matadorsuite-web${TAG} vpcpqmregistry.azurecr.io/matadorsuite-web:latest
+    docker push vpcpqmregistry.azurecr.io/matadorsuite-web:latest
 fi
 
