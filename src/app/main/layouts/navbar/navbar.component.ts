e3d4bc877b9bf854d855b5e70ab1a4f3e6f875ec
punch <punch.pool@gmail.com>
2019-01-17 10:58:07 +0700

    link help and issue

--- src/app/main/layouts/navbar/navbar.component.ts
+++ src/app/main/layouts/navbar/navbar.component.ts
@@ -23,5 +23,9 @@ export class NavbarComponent implements OnInit {
       behavior: 'smooth',
     });
   }
+  gotoWiki() {
+    // tslint:disable-next-line:max-line-length
+    window.open('https://wiki.matadorsuite.com/bin/login/XWiki/XWikiLogin;jsessionid=F25F2D795516752F6D45E63E025A778A?srid=XBNd7kFK&xredirect=%2Fbin%2Fview%2FPortfolio%2520Portal%2520%2528POP%2529%2FHelp%2520Center%2F%3Fsrid%3DXBNd7kFK');
+  }
 
 }
