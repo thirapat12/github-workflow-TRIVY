9bb23a4cf55df0075af08ab9d202d42d65b8bb54
darknamer <darknamer@gmail.com>
2019-01-17 22:35:30 +0700

    add test code

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -4,12 +4,16 @@ import { ShareTestingModule } from '../share-testing.module';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [ShareTestingModule],
-  }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ShareTestingModule],
+    });
+  });
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
   });
+
+
 });
