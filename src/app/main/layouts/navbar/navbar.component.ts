44ec15eb7f099d089472ba0c6039576301616d48
punch <punch.pool@gmail.com>
2019-01-18 09:19:03 +0700

    login ids

--- src/app/main/layouts/navbar/navbar.component.ts
+++ src/app/main/layouts/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,12 +11,29 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class NavbarComponent implements OnInit {
   env: any;
+  userName: string;
+  role: string;
+  login = true;
+  logout = false;
+  showuser = false;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    protected cookie: CookieService,
+    private auth: AuthService
+  ) {
+    this.auth.onChangeUsername.subscribe((userName) => {
+      this.userName = userName;
+    });
+    this.auth.onChangeSsoRole.subscribe((ssoRole) => {
+      this.role = ssoRole;
+      // this.setmenu(this.role);
+    });
   }
 
   ngOnInit() {
     this.env = environment;
+    this.user();
   }
 
   onScroll(id: string) {
@@ -26,10 +44,27 @@ export class NavbarComponent implements OnInit {
   }
 
   gotoWiki() {
+    // tslint:disable-next-line:max-line-length
     window.open('https://wiki.matadorsuite.com/bin/login/XWiki/XWikiLogin;jsessionid=F25F2D795516752F6D45E63E025A778A?srid=XBNd7kFK&xredirect=%2Fbin%2Fview%2FPortfolio%2520Portal%2520%2528POP%2529%2FHelp%2520Center%2F%3Fsrid%3DXBNd7kFK');
   }
 
   onLoginClick() {
     this.router.navigate(['/auth/signin']);
   }
+
+  onlogout() {
+    this.auth.signOut();
+  }
+
+
+  user() {
+    if (this.userName) {
+      this.login = false;
+      this.logout = true;
+    } else {
+      this.login = true;
+      this.logout = false;
+    }
+  }
+
 }
