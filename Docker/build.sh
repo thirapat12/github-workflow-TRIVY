f414615d82957ae885d10a8d4581449d510150dd
wasawat pruthipunyaskul <wasawaz@gmail.com>
2019-01-29 12:07:20 +0700

    fix build.sh

--- Docker/build.sh
+++ Docker/build.sh
@@ -8,9 +8,10 @@ COMPOSE_FILE="${PWD}/Docker/docker-compose.yml"
 docker-compose -f $COMPOSE_FILE build
 docker-compose -f $COMPOSE_FILE push
 
-if [ $BUILD_ENV != "prod"] &&  [$TAG != "latest" ]; then
-    export TAG=":$TAG"
-    docker tag vpcpqmregistry.azurecr.io/matadorsuite-web${TAG} vpcpqmregistry.azurecr.io/matadorsuite-web:latest
-    docker push vpcpqmregistry.azurecr.io/matadorsuite-web:latest
+if  [ "${BUILD_ENV}" = 'prod' ] && [ "${TAG}" != 'latest' ]
+then
+        export TAG=":$TAG"
+        docker tag vpcpqmregistry.azurecr.io/matadorsuite-web${TAG} vpcpqmregistry.azurecr.io/matadorsuite-web:latest
+        docker push vpcpqmregistry.azurecr.io/matadorsuite-web:latest
 fi
 
