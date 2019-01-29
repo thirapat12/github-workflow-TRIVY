9d8b3fa15de7900cf39d473417a42466f0f0a1ac
wasawat pruthipunyaskul <wasawaz@gmail.com>
2019-01-29 12:19:16 +0700

    edit build.sh

--- Docker/build.sh
+++ Docker/build.sh
@@ -3,14 +3,14 @@ set -e
 
 TAG=$1
 BUILD_ENV=$2
+export TAG=":$TAG"
 export BUILD_ENV="$BUILD_ENV"
 COMPOSE_FILE="${PWD}/Docker/docker-compose.yml"
 docker-compose -f $COMPOSE_FILE build
 docker-compose -f $COMPOSE_FILE push
 
-if  [ "${BUILD_ENV}" = 'prod' ] && [ "${TAG}" != 'latest' ]
+if  [ "${BUILD_ENV}" = 'prod' ] && [ "${1}" != 'latest' ]
 then
-        export TAG=":$TAG"
         docker tag vpcpqmregistry.azurecr.io/matadorsuite-web${TAG} vpcpqmregistry.azurecr.io/matadorsuite-web:latest
         docker push vpcpqmregistry.azurecr.io/matadorsuite-web:latest
 fi
