e93e317fc63324817ba8b2919c88fb1c695bd4b8
punch <punch.pool@gmail.com>
2019-02-06 13:37:31 +0700

    update login goto team

--- src/app/modules/page/pages/product-page/product-page.component.ts
+++ src/app/modules/page/pages/product-page/product-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-product-page',
@@ -6,11 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-page.component.scss']
 })
 export class ProductPageComponent implements OnInit {
-
-  constructor() {
+  env: any;
+  constructor(
+    private router: Router,
+  ) {
   }
 
   ngOnInit() {
+    this.env = environment;
   }
 
 }
