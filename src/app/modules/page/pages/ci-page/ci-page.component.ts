9019c645f0c4653b22197dc1eefd4379bf5f43de
Thirapat Amornsin <thira676@tpcon.postbox.in.th>
2019-12-18 10:55:38 +0700

    change ci-page

--- src/app/modules/page/pages/ci-page/ci-page.component.ts
+++ src/app/modules/page/pages/ci-page/ci-page.component.ts
@@ -13,19 +13,19 @@ export class CiPageComponent implements OnInit {
     this.ciList = [
       {
         id: 1,
-        url: 'http://ci.matador.ais.co.th/team-digi/jenkins/',
-        ip: '(Inside Kubernetes Cluster)',
-        description: 'AIS DIGI Team'
-      }, {
-        id: 2,
-        url: 'http://ci.matador.ais.co.th/team-bsstranform/jenkins/',
+        url: 'https://ci.matador.ais.co.th/team-bsstranform/jenkins/',
         ip: '(Inside Kubernetes Cluster)',
         description: 'BSS Transformation Team'
       }, {
-        id: 3,
-        url: 'http://ci.matador.ais.co.th/team-callingmelody/jenkins/',
+        id: 2,
+        url: 'https://ci.matador.ais.co.th/team-callingmelody/jenkins/',
         ip: '(Inside Kubernetes Cluster)',
-        description: 'Callingmelody Team'
+        description: 'Calling Melody Team'
+      }, {
+        id: 3,
+        url: 'https://ci.matador.ais.co.th/team-digi/jenkins/',
+        ip: '(Inside Kubernetes Cluster)',
+        description: 'DIGI Team'
       }, {
         id: 4,
         url: 'http://ci.matador.ais.co.th/dt/',
@@ -33,59 +33,89 @@ export class CiPageComponent implements OnInit {
         description: 'Digital Trading Team'
       }, {
         id: 5,
-        url: 'http://ci.matador.ais.co.th/team-etopup/jenkins/',
+        url: 'https://ci.matador.ais.co.th/team-etopup/jenkins/',
         ip: '(Inside Kubernetes Cluster)',
         description: 'E-Topup Team'
       }, {
         id: 6,
-        url: 'http://ci.matador.ais.co.th/team-ikm/jenkins/',
+        url: 'https://ci.matador.ais.co.th/team-ikm/jenkins/',
         ip: '(Inside Kubernetes Cluster)',
         description: 'IKM Team'
       }, {
         id: 7,
-        url: 'http://ci.matador.ais.co.th/team-iss/jenkins/',
+        url: 'https://ci.matador.ais.co.th/team-iss/jenkins/',
         ip: '(Inside Kubernetes Cluster)',
         description: 'ISS Team'
       }, {
         id: 8,
-        url: 'http://ci.matador.ais.co.th/team-onlinestore/jenkins/',
+        url: 'https://ci.matador.ais.co.th/team-mdprov/jenkins/',
+        ip: '(Inside Kubernetes Cluster)',
+        description: 'MDProvisioning Team'
+      }, {
+        id: 9,
+        url: 'https://ci.matador.ais.co.th/team-myaccount/jenkins/',
+        ip: '(Inside Kubernetes Cluster)',
+        description: 'MyAccount Team'
+      }, { 
+        id: 10,
+        url: 'https://ci.matador.ais.co.th/team-myais/jenkins/',
+        ip: '(Inside Kubernetes Cluster)',
+        description: 'MyAIS Team'
+      }, {
+        id: 11,
+        url: 'https://ci.matador.ais.co.th/team-onlinestore/jenkins/',
         ip: '(Inside Kubernetes Cluster)',
         description: 'Online Store Team'
-      }, { 
-        id: 9,
-        url: 'http://ci.matador.ais.co.th/team-onlinedigitalstore/jenkins/',
+      }, {
+        id: 12,
+        url: 'https://ci.matador.ais.co.th/team-onlinetracking/jenkins/',
         ip: '(Inside Kubernetes Cluster)',
         description: 'Online Tracking Team'
       }, {
-        id: 10,
-        url: 'http://ci.matador.ais.co.th/team-pcr/jenkins/',
-        ip: '(Inside Kubernetes Cluster)',
-        description: 'PCR Team'
-      }, {
-        id: 11,
-        url: 'http://ci.matador.ais.co.th/team-pfsm/jenkins/',
+        id: 13,
+        url: 'https://ci.matador.ais.co.th/team-pfsm/jenkins/',
         ip: '(Inside Kubernetes Cluster)',
         description: 'PFSM Team'
       }, {
-        id: 12,
+        id: 14,
         url: 'http://ci.matador.ais.co.th/phx/',
         ip: '10.138.36.144',
         description: 'Phoenix Team'
       }, {
-        id: 13,
-        url: 'http://ci.matador.ais.co.th/team-siddeploy/jenkins/',
-        ip: '(Inside Kubernetes Cluster)',
-        description: 'SID Deploy Team'
-      }, {
-        id: 14,
-        url: 'http://ci.matador.ais.co.th/team-smart/jenkins/',
-        ip: '(Inside Kubernetes Cluster)',
-        description: 'TC Smart Team'
-      }, {
         id: 15,
+        url: 'https://ci.matador.ais.co.th/team-pqbpammicrom/jenkins/',
+        ip: 'Inside Kubernetes Cluster',
+        description: 'BPAM Microservice Team'
+      }, {
+        id: 16,
+        url: 'https://ci.matador.ais.co.th/team-siddeploy/jenkins/',
+        ip: '(Inside Kubernetes Cluster)',
+        description: 'SIDDeploy Team'
+      }, {
+        id: 17,
+        url: 'http://ci.matador.ais.co.th/team-singularity/jenkins/',
+        ip: '(Inside Kubernetes Cluster)',
+        description: 'Singularity Team'
+      }, {
+        id: 18,
+        url: 'https://ci.matador.ais.co.th/team-smart/jenkins/',
+        ip: '(Inside Kubernetes Cluster)',
+        description: 'TCSmart Team'
+      }, {
+        id: 19,
+        url: 'https://ci.matador.ais.co.th/team-ssb/jenkins/',
+        ip: '(Inside Kubernetes Cluster)',
+        description: 'SSB Team'
+      }, {
+        id: 20,
         url: 'http://ci.matador.ais.co.th/team-mychannel/',
         ip: '10.137.16.242',
-        description: 'My Channel Team'
+        description: 'MyChannel Team'
+      }, {
+        id: 21,
+        url: 'https://ci.matador.ais.co.th/team-vid/jenkins/',
+        ip: '(Inside Kubernetes Cluster)',
+        description: 'Video Integration Development Team'
       }
     ];
   }
